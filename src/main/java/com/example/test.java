1e6486d8f956ef251afe082a976be1525ab871b2
zhihao <2719696703@qq.com>
2023-05-08 08:54:40 +0800

    新增了类test,加了代码

--- src/main/java/com/example/test.java
+++ src/main/java/com/example/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("111111111");
         System.out.println("222222222222");
+        System.out.println("22333333333333");
     }
 
 }
