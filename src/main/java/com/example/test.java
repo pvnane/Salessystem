ccbc4e0c8fa7adccbff62b240c4511e4d3a5dffe
zhihao <2719696703@qq.com>
2023-05-08 08:28:21 +0800

    新增了类test

---
+++ src/main/java/com/example/test.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("111111111");
+        System.out.println("222222222222");
+    }
+
+}
